7ddd5b259648da20e8c372ec2e41b34f0758be3d
aieckxis <aleckxiskate@gmail.com>
2023-04-15 19:40:25 +0800

    pseudocode

---
+++ problem2.py
@@ -0,0 +1,13 @@
+# Aleckxis Kate v. Bernabe
+# BSCpE 1-4
+
+# Problem 2
+# Ask the user to input the encrypted text.
+# Set an empty string for the plain text.
+# Replace each character in encrypted text with corresponding vowel character.
+#   if *, change to a
+#   if &, change to e
+#   if #, change to i
+#   if +, change to o
+#   if !, change to u
+# Display the plain text
