7e1cfe4fea71a461fc2ffc3a831966da25df88eb
aieckxis <aleckxiskate@gmail.com>
2023-04-15 20:14:40 +0800

    replaced the characters

--- problem2.py
+++ problem2.py
@@ -7,20 +7,20 @@ encrypted_text = input("Enter a string to descrypt: ")
 # Set an empty string for the plain text.
 plain_text = ""
 # Replace each character in encrypted text with corresponding vowel character.
-#   if *, change to a
-#   if &, change to e
-#   if #, change to i
-#   if +, change to o
-#   if !, change to u
 for char in encrypted_text:
+#   if *, change to a
     if char == "*":
         plain_text += "a"
+#   if &, change to e
     elif char == "&":
         plain_text += "e"
+#   if #, change to i
     elif char == "#":
         plain_text += "i"
+#   if +, change to o
     elif char == "+":
         plain_text += "o"
+#   if !, change to u
     elif char == "!":
         plain_text += "u"
     else:
